c1c79e7d6088e59313587627b800bebc6141b074
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-30 08:07:08 +0700

    Release v0.001

--- lib/Bencher/Scenario/RefUtil.pm
+++ lib/Bencher/Scenario/RefUtil.pm
@@ -14,26 +14,32 @@ our $scenario = {
         {
             name=>'is_arrayref',
             module => 'Ref::Util',
-            code_template => 'no warnings "void"; state $ref = []; Ref::Util::is_arrayref($ref) for 1..10_000',
+            code_template => 'no warnings "void"; state $ref = []; Ref::Util::is_arrayref($ref) for 1..1000',
         },
         {
             name=>'is_plain_arrayref',
             module => 'Ref::Util',
-            code_template => 'no warnings "void"; state $ref = []; Ref::Util::is_plain_arrayref($ref) for 1..10_000',
+            code_template => 'no warnings "void"; state $ref = []; Ref::Util::is_plain_arrayref($ref) for 1..1000',
         },
         {
             name=>'ref(ARRAY)',
-            code_template => 'no warnings "void"; state $ref = []; ref($ref) eq "ARRAY" for 1..10_000',
+            code_template => 'no warnings "void"; state $ref = []; ref($ref) eq "ARRAY" for 1..1000',
         },
         {
             name=>'reftype(ARRAY)',
             module => 'Scalar::Util',
-            code_template => 'no warnings "void"; state $ref = []; Scalar::Util::reftype($ref) eq "ARRAY" for 1..10_000',
+            code_template => 'no warnings "void"; state $ref = []; Scalar::Util::reftype($ref) eq "ARRAY" for 1..1000',
         },
     ],
 };
 
 1;
 # ABSTRACT:
+
+=head1 BENCHMARK NOTES
+
+I'm not seeing significant performance difference between C<ref() eq "ARRAY">
+and C<is_arrayref()> on my perls. Am I doing something wrong?
+
 
 =head1 SEE ALSO
